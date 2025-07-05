4f91943932c426dbeb1a9ac1a8f07aa544c26d27
LENA-Ted <seralicious.ted@gmail.com>
2025-07-05 20:26:30 +0800

    feat: Updated projectile with range and source tracking.

--- scripts/entities/projectile.js
+++ scripts/entities/projectile.js
@@ -1,9 +1,13 @@
 class Projectile {
-    constructor(x, y, target) {
+    constructor(x, y, target, source_tower = null) {
         this.x = x;
         this.y = y;
+        this.start_x = x;
+        this.start_y = y;
         this.radius = 4;
         this.damage = GAME_CONFIG.PROJECTILE_DAMAGE;
+        this.source_tower = source_tower;
+        this.max_range = source_tower ? source_tower.get_range() : null;
         
         const angle = MathUtils.get_angle_between(x, y, target.x, target.y);
         this.vx = Math.cos(angle) * GAME_CONFIG.PROJECTILE_SPEED;
@@ -28,6 +32,15 @@ class Projectile {
                this.x > canvas_width + margin || 
                this.y < -margin || 
                this.y > canvas_height + margin;
+    }
+
+    is_out_of_range() {
+        if (this.max_range === null) {
+            return false;
+        }
+        
+        const distance = MathUtils.get_distance(this.start_x, this.start_y, this.x, this.y);
+        return distance > this.max_range;
     }
 
     get_damage() {
