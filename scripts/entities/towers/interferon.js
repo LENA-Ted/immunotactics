191ef0d89ef59566b5edf88ac9886e68d67443cc
LENA-Ted <seralicious.ted@gmail.com>
2025-07-05 20:17:12 +0800

    feat: Created interferon.

---
+++ scripts/entities/towers/interferon.js
@@ -0,0 +1,44 @@
+class Interferon extends BaseTower {
+    constructor(x, y, id) {
+        super(x, y, id, IMMUNE_CELL_CONFIGS.INTERFERON);
+    }
+
+    check_activity_criteria() {
+        return true;
+    }
+
+    perform_action(timestamp) {
+        if (!this.can_afford_action()) {
+            return;
+        }
+
+        this.consume_action_cost();
+        this.release_wave();
+    }
+
+    release_wave() {
+        this.create_wave_effect();
+        this.apply_interfered_to_enemies();
+    }
+
+    create_wave_effect() {
+        if (window.game_state && window.game_state.effects) {
+            const wave = new WaveEffect(this.x, this.y, this.range);
+            window.game_state.effects.push(wave);
+        }
+    }
+
+    apply_interfered_to_enemies() {
+        if (!window.game_state || !window.game_state.enemies) {
+            return;
+        }
+
+        window.game_state.enemies.forEach(enemy => {
+            if (this.is_in_range(enemy)) {
+                
+            }
+        });
+    }
+}
+
+window.Interferon = Interferon;
