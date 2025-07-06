7c979f7b7f2a98bcf65aac144bd060cc68d84443
LENA-Ted <seralicious.ted@gmail.com>
2025-07-06 15:06:15 +0800

    fix: Config changes.

--- scripts/entities/status_effects/interfered_effect.js
+++ scripts/entities/status_effects/interfered_effect.js
@@ -1,7 +1,7 @@
 class InterferedEffect extends BaseStatusEffect {
     constructor(duration_ms) {
         super(duration_ms);
-        this.speed_reduction_factor = 0.6;
+        this.speed_reduction_factor = 0.5;
     }
 
     get_type() {
