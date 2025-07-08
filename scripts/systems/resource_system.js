1f7a8d48a218237a88678a96c9243125e363d86f
LENA-Ted <seralicious.ted@gmail.com>
2025-07-08 22:26:37 +0800

    config: Config changes.

--- scripts/systems/resource_system.js
+++ scripts/systems/resource_system.js
@@ -116,7 +116,7 @@ class ResourceSystem {
 
     calculate_resource_gain(intensity_level) {
         const min_gain = 1;
-        const max_gain = Math.max(1, intensity_level);
+        const max_gain = Math.max(1, 1);
         return MathUtils.get_random_int(min_gain, max_gain);
     }
 
