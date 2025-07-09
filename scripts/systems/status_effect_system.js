165f065bff017a53952ee1a0163ff2cf84f74eeb
LENA-Ted <seralicious.ted@gmail.com>
2025-07-09 15:06:30 +0800

    feat: Updated status_effect_system with new Suppressed status methods.

--- scripts/systems/status_effect_system.js
+++ scripts/systems/status_effect_system.js
@@ -16,6 +16,11 @@ class StatusEffectSystem {
         target.add_status_effect(interfered_effect);
     }
 
+    apply_suppressed(target, duration_ms) {
+        const suppressed_effect = new SuppressedEffect(duration_ms);
+        target.add_status_effect(suppressed_effect);
+    }
+
     remove_all_status_effects(target) {
         if (target.status_effects) {
             target.status_effects.forEach(effect => {
@@ -31,6 +36,25 @@ class StatusEffectSystem {
         }
         
         return target.status_effects.filter(effect => effect.get_type() === effect_type);
+    }
+
+    is_target_suppressed(target) {
+        return target.is_suppressed === true || target.has_status_effect('SUPPRESSED');
+    }
+
+    clear_suppressed_status(target) {
+        if (target.is_suppressed !== undefined) {
+            target.is_suppressed = false;
+        }
+        
+        const suppressed_effects = this.get_status_effects_of_type(target, 'SUPPRESSED');
+        suppressed_effects.forEach(effect => {
+            effect.remove_effect(target);
+        });
+        
+        if (target.status_effects) {
+            target.status_effects = target.status_effects.filter(effect => effect.get_type() !== 'SUPPRESSED');
+        }
     }
 }
 
