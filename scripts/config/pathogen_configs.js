72210c73954a218b40920e54f916621603c81374
LENA-Ted <seralicious.ted@gmail.com>
2025-08-09 11:26:03 +0800

    feat: Updated pathogen_configs to include Rickettsia.

--- scripts/config/pathogen_configs.js
+++ scripts/config/pathogen_configs.js
@@ -10,15 +10,31 @@ const PATHOGEN_CONFIGS = {
         color: '#556B2F',
         stroke_color: '#000000',
         stroke_width: 2
+    },
+    RICKETTSIA: {
+        min_hp: 40,
+        max_hp: 40,
+        min_speed: 0.5,
+        max_speed: 0.5,
+        min_size_mod: 1.0,
+        max_size_mod: 1.0,
+        base_radius: 20,
+        color: '#FFA500',
+        stroke_color: '#000000',
+        stroke_width: 2,
+        action_interval_ms: 2000,
+        inoculation_range: 200
     }
 };
 
 const PATHOGEN_TYPES = {
-    MYCETOMA: 'MYCETOMA'
+    MYCETOMA: 'MYCETOMA',
+    RICKETTSIA: 'RICKETTSIA'
 };
 
 const PATHOGEN_POOL = [
-    PATHOGEN_TYPES.MYCETOMA
+    PATHOGEN_TYPES.MYCETOMA,
+    PATHOGEN_TYPES.RICKETTSIA
 ];
 
 window.PATHOGEN_CONFIGS = PATHOGEN_CONFIGS;
